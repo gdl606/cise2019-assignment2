2893b8a890cd341d86a8e11ea1406664393cca57
Anuka <anuka9w@gmail.com>
2019-09-24 15:17:26 +1200

    slider

--- filter.js
+++ filter.js
@@ -1,4 +1,13 @@
+const slider = document.getElementById("price");
+const slabel = document.getElementById("slider_label");
+
+slabel.innerHTML = slider.value; // Display the default slider value
+slider.oninput = function() { // Update the current slider value (each time you drag the slider handle)
+  slabel.innerHTML = this.value;
+}
+
 document.getElementById("filter-search").addEventListener("click", function() {
+
   fetch("/data/treeinfo.json")
     .then(response => response.json())
     .then(data => {
@@ -12,12 +21,15 @@ document.getElementById("filter-search").addEventListener("click", function() {
         // Go through all the dropdown menus and compare their values against the JSON data.
         for (const dropdown of dropdown_list) {
           if (dropdown.value != "" && data[i][dropdown.id] != dropdown.value) {
-            console.log(data[i][dropdown.id] + " doesn't match with " + dropdown.value);
             matching = false;
             break;
           }
         }
         
+        if (data[i][slider.id] > slider.value) {
+          matching = false;
+        }
+
         if (matching) {
           matching_list.push(data[i]);
         }
@@ -30,37 +42,3 @@ document.getElementById("filter-search").addEventListener("click", function() {
       }
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function generateOptions()
-//{
-//    
-//    var name = document.getElementById("name").value;
-//
-//}
-
-//    var category = document.getElementById("Category").value;
-//    var conditions = document.getElementById("Conditions").value;
-//    var maintainence = document.getElementById("Maintainence").value;
-//    var maxheight = document.getElementById("Max Height").value;
-//    var growthrate = document.getElementById("Growth Rate").value;
-//    var price = document.getElementById("Price").value;
