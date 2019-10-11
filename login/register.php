d3e1b21a0b7afd77faee9af2c2d71dac6135ecf4
ss-raicangu <49044752+ss-raicangu@users.noreply.github.com>
2019-10-11 15:15:48 +1300

    Clean up navigation bar on some pages. Fix style paths in login pages.

--- login/register.php
+++ login/register.php
@@ -71,38 +71,86 @@
 ?>
 
 <html>
-<head><title>Register</title></head>
-	<style>
-	html, body {
-		margin: 1px;
-		border: 0;
-	}
-	</style>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Register | Plant A Tree</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="/gulped_modules/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="/gulped_modules/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    
+  <!-- Agency theme core CSS -->
+  <link href="/css/agency.css" rel="stylesheet">
+</head>
 <body>
-	<div align="center">
+	<nav class="navbar navbar-expand-lg navbar fixed-top" id="mainNav">
+      <div class="container">
+          <a class="navbar-brand js-scroll-trigger" href="index.html">Tree Store</a>
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              Menu
+              <i class="fas fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav text-uppercase ml-auto">
+                  <li class="nav-item">
+                      <a class="nav-link js-scroll-trigger" href="/index.html">Home</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link js-scroll-trigger" href="/shop.html">Products</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link js-scroll-trigger" href="/cart.html">Cart</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link js-scroll-trigger" href="/login/?location=/index.html">Login</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </nav>
+  <h1>Register</h1> <!-- TODO FIXME Navigation bar hides this header -->
+  <div class="container">
 		<div style=" border: solid 1px #006D9C; " align="left">
-			<?php
-				if(isset($errMsg)){
-					echo '<div style="color:#FF0000;text-align:center;font-size:17px;">'.$errMsg.'</div>';
-				}
-			?>
-			<div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>Register</b></div>
 			<div style="margin: 15px">
-				<form action="" method="post">
-					<input type="text" name="first-name" placeholder="First Name" value="<?php if(isset($_POST['first-name'])) echo $_POST['first-name'] ?>" autocomplete="off" class="box"/><br /><br />
-					<input type="text" name="last-name" placeholder="Last Name" value="<?php if(isset($_POST['last-name'])) echo $_POST['last-name'] ?>" autocomplete="off" class="box"/><br /><br />
-					<input type="text" name="post-address" placeholder="Postal Address" value="<?php if(isset($_POST['post-address'])) echo $_POST['post-address'] ?>" autocomplete="off" class="box"/><br /><br />
-					<input type="text" name="delivery-address" placeholder="Delivery Address" value="<?php if(isset($_POST['delivery-address'])) echo $_POST['delivery-address'] ?>" autocomplete="off" class="box"/><br /><br />
-					<input type="email" name="email" placeholder="Email*" value="<?php if(isset($_POST['email'])) echo $_POST['email'] ?>" autocomplete="off" class="box" required/><br /><br />
-					<input type="text" name="phone" placeholder="Phone" value="<?php if(isset($_POST['phone'])) echo $_POST['phone'] ?>" autocomplete="off" class="box"/><br /><br />
-					<input type="text" name="username" placeholder="Username*" value="<?php if(isset($_POST['username'])) echo $_POST['username'] ?>" autocomplete="off" class="box" required/><br /><br />
-					<input type="password" name="password" placeholder="Password*" value="<?php if(isset($_POST['password'])) echo $_POST['password'] ?>" class="box" required/><br/><br />
-          <!--TODO: Confirm Password text field-->
-					<input type="submit" name='register' value="Register" class='submit'/><br />
+				<form method="post">
+          <!-- Echoing the value so that the boxes are auto-filled in-case there is an error -->
+        <input class="form-control" type="text" name="first-name" value="<?php if (isset($_POST['first-name'])) { echo $_POST['first-name']; } ?>" placeholder="First Name"/><br /><br />
+					<input class="form-control" type="text" name="last-name" value="<?php if (isset($_POST['last-name'])) { echo $_POST['last-name']; } ?>" placeholder="Last Name"/><br /><br />
+					<input class="form-control" type="text" name="post-address" value="<?php if (isset($_POST['post-address'])) { echo $_POST['post-address']; } ?>" placeholder="Postal Address"/><br /><br />
+					<input class="form-control" type="text" name="delivery-address" value="<?php if (isset($_POST['delivery-address'])) { echo  $_POST['delivery-address']; } ?>" placeholder="Delivery Address"/><br /><br />
+					<input class="form-control" type="email" name="email" value="<?php if (isset($_POST['email'])) { echo  $_POST['email']; } ?>" placeholder="Email*" required/><br /><br />
+					<input class="form-control" type="text" name="phone" value="<?php if (isset($_POST['phone'])) { echo  $_POST['phone']; } ?>" placeholder="Phone"/><br /><br />
+					<input class="form-control" type="text" name="username" value="<?php if (isset($_POST['username'])) { echo  $_POST['username']; } ?>" placeholder="Username*" required/><br /><br />
+					<input class="form-control" type="password" name="password" value="<?php if (isset($_POST['password'])) { echo  $_POST['password']; } ?>" placeholder="Password*" autocomplete="off" required/><br/><br />
+          <!--TODO Confirm Password text field-->
+					<input type="submit" name='register' value="Register" class='btn btn-primary submit'/><br />
 				</form>
+      <?php
+				if(isset($errMsg)){
+					echo '<span style="color:#FF0000;text-align:center;font-size:17px;">'.$errMsg.'</span>';
+				}
+      ?>
 			</div>
-		</div>
-	</div>
+    </div>
+  </div>
+  
+  <script src="../gulped_modules/jquery/jquery.min.js"></script>
+  <script src="../gulped_modules/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Plugin JavaScript -->
+  <script src="../gulped_modules/jquery-easing/jquery.easing.min.js"></script>
+  <!-- Contact form JavaScript -->
+  <script src="../js/jqBootstrapValidation.js"></script>
+  <script src="../js/contact_me.js"></script>
+  <!-- Custom scripts for this template -->
+  <script src="../js/agency.min.js"></script>
 </body>
 </html>
 
