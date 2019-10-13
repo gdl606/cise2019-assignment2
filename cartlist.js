3aa80f3180bc1c114322d3328f966a0fbaaa90ee
Anuka <anuka9w@gmail.com>
2019-10-13 13:45:43 +1300

    Fix remove item functionality in the cart

--- cartlist.js
+++ cartlist.js
@@ -1,120 +1,111 @@
-//var url = new URL(window.location.href);
-//var name = url.searchParams.get("name");
-//var price = url.searchParams.get("price");
-//document.getElementById("prodname").innerHTML = name;
-//document.getElementById("prodprice").innerHTML = price;
-
-var title = [];
-var prices = [];
-var floatPrices = [];
-var totalPrice;
+let titles = [];
+let prices = [];
+let floatPrices = [];
+let totalPrice;
 
 for(let i=0 ; i<sessionStorage.length; i++)// session storage temporary storage takes all elements in that storage to use in the next loop
 {
-
-    var key = sessionStorage.key(i);
-    title[i] = key;
+    const key = sessionStorage.key(i);
+    titles[i] = key;
     prices[i] = sessionStorage.getItem(key);
 
-    var oneString = prices[i];
-    var smalString = oneString.slice(1, 7);
-    var oneprice = parseFloat(smalString);
-    floatPrices[i] = oneprice;
+    const onePrice = parseFloat(prices[i].slice(1, 7));
+    floatPrices[i] = onePrice;
 }
 
-
-console.log(title);
-console.log(prices);
-
-var id=0;
-// adds the title and price to the html file
-for(let i=0; i<title.length; i++)
+const itemsdiv = document.getElementById("items");
+let id=0;
+for(let i=0; i<titles.length; i++) // adds the titles and price to the html file
 {
     id+=1;
-    var itemtitle = document.createElement("h5"); // create element will create an h5 element in the document (html file)
-    itemtitle.setAttribute("id", id)
-  //    name.setAttribute("id", "item"+itemnum);
-//    ;
-    itemtitle.setAttribute("innnerHTML", title[i]);
-    document.body.appendChild(itemtitle);
+
+    itemsdiv.appendChild(document.createElement("br"));
+
+    const item = document.createElement("div");
+    item.id = id;
+    item.className = "card";
+    item.style = "width: 18em"
+    itemsdiv.appendChild(item);
+
+    const title = document.createElement("h5"); // create element will create an h5 element in the document (html file)
+    title.className = "card-title"
+    title.innerHTML = titles[i];
+    item.appendChild(title);
 
     const price = document.createElement("h6");
     price.innerHTML = prices[i];
-    price.compareDocumentPosition(itemtitle);
-    console.log(i);
-    itemtitle.appendChild(price);
+    item.appendChild(price);
 
-    var button = document.createElement("BUTTON");
-    button.setAttribute("id", id)
+    const button = document.createElement("button");
     button.innerHTML = "Remove";
-    itemtitle.appendChild(button);
-    button.addEventListener("click", itemtitle => removeItem(itemtitle))
+    button.className = "btn btn-primary btn-m text-uppercase";
+    button.style = "align-content: center";
+    item.appendChild(button);
+
+    button.addEventListener("click", () => removeItem(item));
 }
 
-function removeItem(){
-  //console.log(this)
-  var buttonid = button.getAttribute("id")
-  console.log(button)
-
-  //button.parentElement.parentElement.removeChild(document.getElementById(buttonid));
-  console.log(title[0])
-
-  console.log(itemtitle.innerHTML)
-  sessionStorage.removeItem(title[0])
-  var t = button.parentElement;
-  //console.log("sdfd");
-  button.parentElement.parentElement.removeChild(t);
-  //console.log(sessionStorage.key(0));
+// Calculate total price
+let sum = 0.0;
+for(let i = 0; i < floatPrices.length; i++) {
+  sum += floatPrices[i];
 }
-
-var sum = 0.0;
-
-for(var i = 0; i < floatPrices.length; i++)
-    {
-        sum += floatPrices[i];
-    }
-console.log(sum);
 sum = sum.toFixed(2);
-var gstSum = sum*0.15;
+
+let gstSum = sum*0.15;
 gstSum = gstSum.toFixed(2);
-var sumString = sum.toString();
-var gstString = gstSum.toString();
 
-var line3 = document.createElement("h1");
+// Create bottom display
+const line3 = document.createElement("h1");
 line3.innerHTML = "-------------";
-document.body.appendChild(line3);
 
-var deliveryTotal = document.createElement("h5");
+const deliveryTotal = document.createElement("h5");
 deliveryTotal.innerHTML = "Delivery:"
-document.body.appendChild(deliveryTotal);
 
-// awaiting quantity to determine delivery
-var deliveryTotalPrinted = document.createElement("h6");
-deliveryTotalPrinted.innerHTML = "$"
-document.body.appendChild(deliveryTotalPrinted);
+//__________________________________________________________________________________________________ Awaiting quantity to determine delivery
+const deliveryTotalPrinted = document.createElement("h6");
+deliveryTotalPrinted.innerHTML = "$";
 
-var line1 = document.createElement("h1");
+const line1 = document.createElement("h1");
 line1.innerHTML = "-------------";
-document.body.appendChild(line1);
 
-var gstTotal = document.createElement("h5");
-gstTotal.innerHTML = "GST:"
-document.body.appendChild(gstTotal);
+const gstTotal = document.createElement("h5");
+gstTotal.innerHTML = "GST:";
 
-var gstTotalPrinted = document.createElement("h6");
+const gstTotalPrinted = document.createElement("h6");
 gstTotalPrinted.innerHTML = "$"+gstSum;
-document.body.appendChild(gstTotalPrinted);
 
-var line2 = document.createElement("h1");
+const line2 = document.createElement("h1");
 line2.innerHTML = "-------------";
+
+const totalPriceTitle = document.createElement("h5");
+totalPriceTitle.innerHTML = "Total Price: ";
+
+const totalPricePrinted = document.createElement("h6");
+totalPricePrinted.innerHTML = "$"+sum;
+//__________________________________________________________________________________________________
+
+function removeItem(item) {
+  itemID = parseInt(item.id);
+  item.parentElement.removeChild(item);
+  
+  let title = titles[itemID-1];
+  sum -= parseFloat(prices[itemID-1].substr(1));
+  gstSum = sum * 0.15;
+  sum = sum.toFixed(2);
+  gstSum = gstSum.toFixed(2);
+
+  sessionStorage.removeItem(title);
+  gstTotalPrinted.innerHTML = "$" + gstSum;
+  totalPricePrinted.innerHTML = "$" + sum;
+}
+
+document.body.appendChild(line3);
+document.body.appendChild(deliveryTotal);
+document.body.appendChild(deliveryTotalPrinted);
+document.body.appendChild(line1);
+document.body.appendChild(gstTotal);
+document.body.appendChild(gstTotalPrinted);
 document.body.appendChild(line2);
-
-var totalPriceTitle = document.createElement("h5");
-totalPriceTitle.innerHTML = "Total Price: "
 document.body.appendChild(totalPriceTitle);
-
-var totalPricePrinted = document.createElement("h6");
-totalPricePrinted.innerHTML = "$"+sumString;
 document.body.appendChild(totalPricePrinted);
-
-
